a6f0e62618d12daa01500e6d87e34c7590351348
AdityaShriwas18 <147526826+AdityaShriwas18@users.noreply.github.com>
2024-04-16 08:26:23 -0700

    comparisions in javascript

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -5,6 +5,8 @@
 let name = "Aditya"; // string 
 let age = 19;        // number
 let isLoggedIn = true; // boolean => true/false
+let bignumber = 498237498237587238478n // bigInt 
+console.log(typeof bignumber);
 
 //primitive data types
 // string 
@@ -20,3 +22,27 @@ console.log(typeof undefined); //undefined
 // non primitive data types
 // object
 // arrays
+
+const Id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(Id == anotherId);
+
+let friends = ["Aditya", "Aman", "Himanshu", "Kishan"];
+console.table(friends);
+console.log(typeof friends);
+
+
+let data = {
+    name: "Aditya Shriwas",
+    age: 19,
+    city: "Indore",
+    isAdult: true
+}
+
+console.table(data);
+console.log(typeof data);
+
+let func = function MyFunction() {
+    console.log("Namaste Duniya");
+}
+console.log(typeof func);
---
+++ comparision.js
@@ -0,0 +1,12 @@
+// console.log("2" > 0);
+// console.log("2" < 0);
+// console.log("02" >= 0);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+// === is used for strictly check
+
+console.log("2" === 0);
