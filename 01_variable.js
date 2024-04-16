f8161e52ba7283d223cd813d9027eab23511ddc5
AdityaShriwas18 <147526826+AdityaShriwas18@users.noreply.github.com>
2024-04-16 06:59:53 -0700

    datatypes

---
+++ 01_variable.js
@@ -0,0 +1,13 @@
+const myId = 071809;
+let name = "aditya Shriwas";
+var city = "indore";
+let email = "aditya@gmail.com";
+let age;
+// myId = 07; //not allowed
+console.log(myId);
+name = "Aman Rai";
+city = "kshipra";
+email = "aman@gmail.com";
+
+
+console.table([myId, name, city, email, age]);
---
+++ 02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer verison
+
+// alert("hello"); //can't use alert in nodejs
+
+let name = "Aditya"; // string 
+let age = 19;        // number
+let isLoggedIn = true; // boolean => true/false
+
+//primitive data types
+// string 
+// number => range is 2^53
+// boolean
+// bigInt
+// null => standalone value
+// symbol => unique
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
+// non primitive data types
+// object
+// arrays
