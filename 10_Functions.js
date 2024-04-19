6423ebcf41cb906c52f3f607ab6fd1fce60ea42e
AdityaShriwas18 <147526826+AdityaShriwas18@users.noreply.github.com>
2024-04-19 08:26:38 -0700

    arrow functions

--- 10_Functions.js
+++ 10_Functions.js
@@ -62,3 +62,66 @@ console.log(returnSecondElem(array));
 // another way
 
 console.log(returnSecondElem([100, 200, 300, 400]));
+
+// arrow functions and this keyword
+
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
+
